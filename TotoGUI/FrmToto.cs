8e1358605ede9b49adeedd0c7f9f9c5f8876a841
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2023-10-17 19:51:45 +0200

    updt

--- TotoGUI/FrmToto.cs
+++ TotoGUI/FrmToto.cs
@@ -2,6 +2,8 @@ namespace TotoGUI
 {
     public partial class FrmToto : Form
     {
+        const string valid = "12X";
+
         public FrmToto()
         {
             InitializeComponent();
@@ -14,12 +16,10 @@ namespace TotoGUI
             chbKarSzamHiba.Text = $"Nem megfelelõ karakterszám({txtEredmenyek.Text.Length})";
 
             string invalid = string.Empty;
-            foreach (char c in txtEredmenyek.Text)
-                if (!"12X".Contains(c)) invalid += $"{c};";
-
-            if (invalid.Length != 0) invalid = invalid[..^1];
+            foreach (char c in txtEredmenyek.Text) if (!valid.Contains(c)) invalid += $"{c};";
             chbHelytelenKarHiba.Checked = invalid.Length != 0;
-            chbHelytelenKarHiba.Text = $"Helytelen karakter az eredményekben ({invalid})";
+            chbHelytelenKarHiba.Text = $"Helytelen karakter az eredményekben " +
+                $"({(invalid.Length == 0 ? string.Empty : invalid[..^1])})";
 
             btnMentes.Enabled = !chbKarSzamHiba.Checked && !chbHelytelenKarHiba.Checked;
         }
